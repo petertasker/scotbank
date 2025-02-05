f43f689a6ee0d460395369467009473f584d4a90
Peter Tasker <petertasker05@gmail.com>
2025-02-05 16:49:27 +0000

    Implement deposit and withdraw junits

--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -6,10 +6,25 @@ import org.junit.jupiter.api.Test;
 public class AccountTests {
 
     @Test
-    public void createAccount(){
+    public void createAccount() {
         Account a = new Account();
         Assertions.assertTrue(a != null);
     }
 
+    @Test
+    public void addFunds() {
+        Account a = new Account();
+        a.deposit(20);
 
+        a.deposit(50);
+        Assertions.assertEquals(70, a.getBalance());
+    }
+
+    @Test
+    public void addFunds2() {
+        Account a = new Account();
+        a.deposit(40);
+        a.withdraw(20);
+        Assertions.assertEquals(20, a.getBalance());
+    }
 }
