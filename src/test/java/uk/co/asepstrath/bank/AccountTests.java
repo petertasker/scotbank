a95a23ffe27b2bea8eda9035cdddc9867e9eb60d
Peter Tasker CS2023 <peter.tasker.2023@uni.strath.ac.uk>
2025-02-05 19:19:41 +0000

    implement lab 3 test cases

--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -1,30 +1,61 @@
 package uk.co.asepstrath.bank;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AccountTests {
-
+    /* A new Account should have a value of 0 */
     @Test
-    public void createAccount() {
-        Account a = new Account();
-        Assertions.assertTrue(a != null);
+    void createAccount() {
+        Account a = new Account(BigDecimal.ZERO);
+        assertNotNull(a);
+    }
+
+    /* Depositing £50 in an account with £20 should result in an account containing £70 */
+    @Test
+    void addFunds() {
+        Account a = new Account(BigDecimal.valueOf(20));
+
+        a.deposit(BigDecimal.valueOf(50));
+        assertEquals(BigDecimal.valueOf(70), a.getBalance());
     }
 
     @Test
-    public void addFunds() {
-        Account a = new Account();
-        a.deposit(20);
-
-        a.deposit(50);
-        Assertions.assertEquals(70, a.getBalance());
+    /* Withdrawing £20 from an account with £40 should result in an account containing £20 */
+    void addFunds2() {
+        Account a = new Account(BigDecimal.valueOf(40));
+        a.withdraw(BigDecimal.valueOf(20));
+        assertEquals(BigDecimal.valueOf(20), a.getBalance());
     }
 
     @Test
-    public void addFunds2() {
-        Account a = new Account();
-        a.deposit(40);
-        a.withdraw(20);
-        Assertions.assertEquals(20, a.getBalance());
+    /* Withdrawing £100 from an account with £30 should throw an ArithmeticException */
+    void illegalWithdraw() {
+        Account a = new Account(BigDecimal.valueOf(30));
+        assertThrows(ArithmeticException.class, () -> a.withdraw(BigDecimal.valueOf(100)));
+    }
+
+    @Test
+    /* Starting with an account with £20, deposit £10 five times then withdraw £20 three times.
+    The account should end with £10*/
+    void complexDepositAndWithdraw() {
+        Account a = new Account(BigDecimal.valueOf(20));
+        for (int i = 0; i < 5; i++) {
+            a.deposit(BigDecimal.valueOf(10));
+        }
+        for (int i = 0; i < 3; i++) {
+            a.withdraw(BigDecimal.valueOf(20));
+        }
+        assertEquals(BigDecimal.valueOf(10), a.getBalance());
+    }
+    @Test
+    /* Depositing £17.56 into an account with £5.45 should result in an account containing £23.01*/
+    void floatWithdraw() {
+        Account a = new Account(BigDecimal.valueOf(5.45));
+        a.deposit(BigDecimal.valueOf(17.56));
+        assertEquals(BigDecimal.valueOf(23.01), a.getBalance());
     }
 }
