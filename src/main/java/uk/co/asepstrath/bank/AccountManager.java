ec2e88d4511cb8dd11b5e0a28ec9ef47cee71c13
Mohammad Rayyan Adhoni CSM2023 <mohammad.adhoni.2023@uni.strath.ac.uk>
2025-02-05 21:02:23 +0000

    AccountManager.java

---
+++ src/main/java/uk/co/asepstrath/bank/AccountManager.java
@@ -0,0 +1,19 @@
+package uk.co.asepstrath.bank;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountManager {
+
+    public static void onStart(){
+        List<Account> accounts = new ArrayList<Account>();
+
+        accounts.add(new Account("Rachel", BigDecimal.valueOf(50.00)));
+        accounts.add(new Account("Monica", BigDecimal.valueOf(100.00)));
+        accounts.add(new Account("Phoebe", BigDecimal.valueOf(76.00)));
+        accounts.add(new Account("Joey", BigDecimal.valueOf(23.90)));
+        accounts.add(new Account("Chandler", BigDecimal.valueOf(3.00)));
+        accounts.add(new Account("Ross", BigDecimal.valueOf(54.32)));
+    }
+}
