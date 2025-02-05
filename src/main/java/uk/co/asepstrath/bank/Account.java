7e52dbf14e299ff8494cd9104b3f1ed78ece1ad6
Mohammad Rayyan Adhoni CSM2023 <mohammad.adhoni.2023@uni.strath.ac.uk>
2025-02-05 18:43:53 +0000

    Edit Account.java

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -2,13 +2,36 @@ package uk.co.asepstrath.bank;
 
 public class Account {
 
+    private int AccBalance;
+
+    public Account() {
+        this.AccBalance = 0;
+    }
+
+    public Account(int accBalance) {
+        this.AccBalance = accBalance;
+    }
 
     public void deposit(int amount) {
+        if (amount > 0){
+            AccBalance += amount;
+        }else{
+            System.out.println("Deposit amount needs to be greater than 0");
+        }
+    }
 
+    public void Withdraw(int amount) throws ArithmeticException{
+        if (amount < AccBalance){
+            AccBalance -= amount;
+        }else if(amount <= 0){
+            System.out.println("Withdraw amount needs to be greater than 0");
+        } else {
+            throw new ArithmeticException("Insufficient Funds: cannot withdrawal amount more than available balance");
+        }
     }
 
     public int getBalance() {
-        return 0;
+        return AccBalance;
     }
 
 }
