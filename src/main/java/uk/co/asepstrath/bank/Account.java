94a96d3784bdc9fa5e78697013e5dea07ae47dda
Peter Tasker <petertasker05@gmail.com>
2025-02-12 15:42:04 +0000

    Fix merge

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -17,35 +17,22 @@ public class Account {
 
 
     public void deposit(BigDecimal amount) {
-        balance = balance.add(amount);
-
-    public Account(String Account_Name, BigDecimal accBalance) {
-        this.AccBalance = accBalance;
-        this.AccName = Account_Name;
-    }
-
-    public void deposit(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) > 0){
-            AccBalance = AccBalance.add(amount);
+            balance = balance.add(amount);
         }else{
             System.out.println("Deposit amount needs to be greater than 0");
         }
     }
 
-    public void Withdraw(BigDecimal amount) throws ArithmeticException{
-        if (amount.compareTo(AccBalance) > 0){ // if amount is greater than current Balance throw exception
+    public void withdraw(BigDecimal amount) throws ArithmeticException{
+        if (amount.compareTo(balance) > 0){ // if amount is greater than current Balance throw exception
             throw new ArithmeticException("Insufficient Funds: cannot withdrawal amount more than available balance");
         }else if(amount.compareTo(BigDecimal.ZERO) <= 0){ // if withdral amount is less or equal to 0
             System.out.println("Withdraw amount needs to be greater than 0");
         } else {
-           AccBalance = AccBalance.subtract(amount);
+           balance = balance.subtract(amount);
         }
     }
-
-    public BigDecimal getBalance() {
-        return AccBalance;
-    }
-
 
     public BigDecimal getBalance() {
         return balance;
--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -1,5 +1,6 @@
 package uk.co.asepstrath.bank;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,29 +11,29 @@ public class AccountTests {
     /* A new Account should have a value of 0 */
     @Test
     public void createAccount(){
-        Account a = new Account();
-        Assertions.assertNotNull(a);
+        Account a = new Account("John Smith", BigDecimal.valueOf(15));
+        assertNotNull(a);
     }
     
     @Test
     public void deposit(){
         Account a = new Account("Jacob",BigDecimal.valueOf(20));
         a.deposit(BigDecimal.valueOf(50)); // deposit a 50 in a 20
-        Assertions.assertEquals(BigDecimal.valueOf(70),a.getBalance());
+        assertEquals(BigDecimal.valueOf(70),a.getBalance());
     }
 
     @Test
     public void Withdraw(){
         Account a = new Account("Simon",BigDecimal.valueOf(40));
-        a.Withdraw(BigDecimal.valueOf(20)); // withdraw 20 pounds
-        Assertions.assertEquals(BigDecimal.valueOf(20), a.getBalance());
+        a.withdraw(BigDecimal.valueOf(20)); // withdraw 20 pounds
+        assertEquals(BigDecimal.valueOf(20), a.getBalance());
     }
 
     @Test
     public void TestArithmeticException(){
-        Account a = new Account();
+        Account a = new Account("John Smith",BigDecimal.valueOf(20));
         a.deposit(BigDecimal.valueOf(30));
-        Assertions.assertThrows(ArithmeticException.class, () -> a.Withdraw(BigDecimal.valueOf(100)));
+        Assertions.assertThrows(ArithmeticException.class, () -> a.withdraw(BigDecimal.valueOf(100)));
     }
 
     @Test
@@ -42,20 +43,16 @@ public class AccountTests {
             a.deposit(BigDecimal.valueOf(10));
         }
         for (int i=0; i<3; i++){
-            a.Withdraw(BigDecimal.valueOf(20));
+            a.withdraw(BigDecimal.valueOf(20));
         }
-        Assertions.assertEquals(BigDecimal.valueOf(10),a.getBalance());
+        assertEquals(BigDecimal.valueOf(10),a.getBalance());
     }
 
     @Test
     public void Testforpennies(){
         Account a = new Account("Jack",BigDecimal.valueOf(5.45));
         a.deposit(BigDecimal.valueOf(17.56));
-        Assertions.assertEquals(BigDecimal.valueOf(23.01), a.getBalance());
-    }
-
-        a.deposit(BigDecimal.valueOf(50));
-        assertEquals(BigDecimal.valueOf(70), a.getBalance());
+        assertEquals(BigDecimal.valueOf(23.01), a.getBalance());
     }
 
     @Test
