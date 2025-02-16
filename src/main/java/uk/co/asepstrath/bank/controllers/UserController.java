043024179f00908c2ae00c04a11391d109f9f81b
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-02-16 22:09:20 +0000

    unfinished: process user into session JSON object

--- src/main/java/uk/co/asepstrath/bank/controllers/UserController.java
+++ src/main/java/uk/co/asepstrath/bank/controllers/UserController.java
@@ -1,5 +1,8 @@
 package uk.co.asepstrath.bank.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jooby.Context;
 import io.jooby.ModelAndView;
 import io.jooby.StatusCode;
 import io.jooby.annotation.*;
@@ -8,6 +11,7 @@ import kong.unirest.core.Unirest;
 import org.slf4j.Logger;
 import uk.co.asepstrath.bank.Account;
 import uk.co.asepstrath.bank.AccountManager;
+import uk.co.asepstrath.bank.User;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -50,7 +54,29 @@ public class UserController {
         return new ModelAndView("registerform.hbs", model);
     }
 
+    /**
+     * Register a new user and add to session as JSON object
+     */
+    @POST
+    @Path("/register")
+//    @Consumes("application/json") // Take in a JSON object
+//    @Produces("application/json") // Produce a JSON object
+    public User registerUser(Context ctx) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> formData = new HashMap<>();
+            formData.put("fullname", ctx.form("fullname").value());
+            formData.put("email", ctx.form("email").value());
+            String jsonString = mapper.writeValueAsString(formData); // Map register data into JSON object cast to string
+            User user = mapper.readValue(jsonString, User.class); // Create java user and put into the session
+            ctx.session().put("user", jsonString);
 
+            return user;
+        }
+        catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 //
 //    @GET("/listaccounts")
 //    public List<Account> getAccounts() {
