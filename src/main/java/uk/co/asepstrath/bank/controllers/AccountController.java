53186ce51214d9abe79bffd14ef51f270ebb812f
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-02-20 18:49:52 +0000

    create controllers for log in page and accounts page

--- src/main/java/uk/co/asepstrath/bank/controllers/AccountController.java
+++ src/main/java/uk/co/asepstrath/bank/controllers/AccountController.java
@@ -1,71 +1,42 @@
-//package uk.co.asepstrath.bank.controllers;
-//
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import io.jooby.Context;
-//import io.jooby.ModelAndView;
-//import io.jooby.annotation.GET;
-//import io.jooby.annotation.POST;
-//import io.jooby.annotation.Path;
-//import io.jooby.annotation.PathParam;
-//import org.slf4j.Logger;
-//import uk.co.asepstrath.bank.Account;
-//
-//import javax.sql.DataSource;
-//import java.math.BigDecimal;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//
-//@Path("/account")
-//public class AccountController {
-//
-//    private final DataSource dataSource;
-//    private final Logger logger;
-////    private final ContextManager contextManager;
-////
-//   public AccountController(DataSource dataSource, Logger logger) {
-//        this.dataSource = dataSource;
-//        this.logger = logger;
-//        // this.contextManager = new ContextManager();
-//        logger.info("Account Controller initialised");
-////    }
-////
-////    @GET
-////    @Path("/{accountid}")
-////    public ModelAndView getAccount(@PathParam("accountid") String accountid) {
-////        Map<String, Object> model = new HashMap<>();
-////        model.put("account", accountid);
-////        return new ModelAndView("accountview.hbs", model);
-////    }
-////
-////    /**
-////     * Open a new account
-////     */
-////    @GET
-////    @Path("/openaccount")
-////    public ModelAndView displayOpenAccount() {
-////        Map<String, Object> model = new HashMap<>();
-////        return new ModelAndView("openaccount.hbs", model);
-////    }
-////
-////    /**
-////     * Open a new account
-////     */
-////    @POST
-////    @Path("/create")
-////    public void openAccount(Context ctx) throws JsonProcessingException {
-////        try {
-////            String formBalance = ctx.form("startingbalance").value();
-////            BigDecimal balance = new BigDecimal(formBalance);
-////            Customer customer = contextManager.getCustomerFromContext(ctx);
-////            Account account = customer.createAccount(balance);
-////            contextManager.addAccountToContext(account, ctx);
-////            ctx.sendRedirect("/dashboard");
-////
-////        } catch (Exception e) {
-////            logger.error("Failed to put account into context", e);
-////            throw e;
-////        }
-////
-////    }
-//}}
+package uk.co.asepstrath.bank.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.jooby.Context;
+import io.jooby.ModelAndView;
+import io.jooby.Session;
+import io.jooby.annotation.GET;
+import io.jooby.annotation.POST;
+import io.jooby.annotation.Path;
+import io.jooby.annotation.PathParam;
+import org.slf4j.Logger;
+import uk.co.asepstrath.bank.Account;
+import uk.co.asepstrath.bank.ContextManager;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Path("/account")
+public class AccountController {
+
+    private final DataSource dataSource;
+    private final Logger logger;
+    private final ContextManager contextManager = new ContextManager();
+
+   public AccountController(DataSource dataSource, Logger logger) {
+       this.dataSource = dataSource;
+       this.logger = logger;
+       logger.info("Account Controller initialised");
+   }
+
+   @GET
+   public ModelAndView viewAccount(Context ctx) {
+       Map<String, Object> model = new HashMap<>();
+       Session session = ctx.session();
+       model.put("name", session.get("name"));
+       model.put("accountid", session.get("accountid"));
+       return new ModelAndView("account.hbs", model);
+   }
+}
