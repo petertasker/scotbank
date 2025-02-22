7e6729f69764c2fff9e1fdbc9f181abb285d1577
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-02-22 10:49:53 +0000

    implement non functional withdraw and deposit pages

--- src/main/java/uk/co/asepstrath/bank/Constants.java
+++ src/main/java/uk/co/asepstrath/bank/Constants.java
@@ -8,11 +8,20 @@ public final class Constants {
 
     public static final String URL_PAGE_LOGIN = "login_user.hbs";
     public static final String URL_PAGE_ACCOUNT = "account.hbs";
+    public static final String URL_PAGE_ACCOUNT_DEPOSIT = "deposit.hbs";
+    public static final String URL_PAGE_ACCOUNT_WITHDRAW = "withdraw.hbs";
+
+    // NOTE: If you refactor these make sure that they are reflected in the handlebars templates,
+    // the variables there don't use these constants.
     public static final String URL_ERROR_MESSAGE = "error";
+
     public static final String SESSION_ACCOUNT_ID = "accountid";
     public static final String SESSION_ACCOUNT_NAME = "name";
+
     public static final String TRANSACTION_OBJECT_LIST = "transactions";
     public static final String TRANSACTION_OBJECT_EXISTS = "hastransactions";
+
+    public static final String ACCOUNT_OBJECT_BALANCE = "balance";
 }
 
 
