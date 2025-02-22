d3d29a6f9840b3e6e9ecc97733d741223ad9bde7
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-02-22 10:22:31 +0000

    refactor constant names to be clearer in intention

--- src/main/java/uk/co/asepstrath/bank/controllers/AccountController.java
+++ src/main/java/uk/co/asepstrath/bank/controllers/AccountController.java
@@ -35,8 +35,8 @@ public class AccountController {
     public ModelAndView<Map<String, Object>> viewAccount(Context ctx) throws SQLException {
         Map<String, Object> model = new HashMap<>();
         Session session = ctx.session();
-        model.put(URL_ACCOUNT_NAME, session.get("name"));
-        model.put(URL_ACCOUNT_ID, session.get("accountid"));
+        model.put(SESSION_ACCOUNT_NAME, session.get("name"));
+        model.put(SESSION_ACCOUNT_ID, session.get("accountid"));
         logger.info("Put name and accountid in model");
 
         // Get all transactions related to a user's account
@@ -49,7 +49,7 @@ public class AccountController {
                             "FROM Transactions " +
                             "WHERE SenderID = ?"
             )) {
-                preparedStatement.setString(1, String.valueOf(session.get(URL_ACCOUNT_ID)));
+                preparedStatement.setString(1, String.valueOf(session.get(SESSION_ACCOUNT_ID)));
 
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
@@ -70,8 +70,8 @@ public class AccountController {
                 }
             }
 
-            model.put(URL_TRANSACTION_OBJECTS, transactions);
-            model.put(URL_TRANSACTION_HAS_OBJECTS, !transactions.isEmpty());
+            model.put(TRANSACTION_OBJECT_LIST, transactions);
+            model.put(TRANSACTION_OBJECT_EXISTS, !transactions.isEmpty());
         }
         return new ModelAndView<>(URL_PAGE_ACCOUNT, model);
     }
