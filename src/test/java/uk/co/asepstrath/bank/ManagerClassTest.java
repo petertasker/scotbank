3187588fe340e7ee2d814b239f73d96a54b054fe
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-03-02 09:52:58 +0000

    create manager class tests

---
+++ src/test/java/uk/co/asepstrath/bank/ManagerClassTest.java
@@ -0,0 +1,30 @@
+package uk.co.asepstrath.bank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ManagerClassTest {
+
+    private Manager manager;
+
+    @BeforeEach void setUp() {
+        manager = new Manager("12345", "Mr. Manager");
+    }
+
+    @Test
+    void testGetManagerID() {
+        assertEquals("12345", manager.getManagerID());
+    }
+
+    @Test
+    void testGetManagerName() {
+        assertEquals("Mr. Manager", manager.getName());
+    }
+
+    @Test
+    void testGetManagerToString() {
+        assertEquals("id: 12345" + System.lineSeparator() + "name: Mr. Manager", manager.toString());
+    }
+}
