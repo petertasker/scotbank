f53e1f3d470309c33a14635d77b095f0718de269
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-03-09 15:00:46 +0000

    properly comment out comment

--- src/test/java/uk/co/asepstrath/bank/parsers/XmlParserTest.java
+++ src/test/java/uk/co/asepstrath/bank/parsers/XmlParserTest.java
@@ -19,14 +19,14 @@ package uk.co.asepstrath.bank.parsers;
 //        xmlMapper = new XmlMapper();
 //    }
 //
-/// /    @Test
-/// /    public void testXmlParserFromFile() throws IOException {
-/// /        File xmlFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("transactionsxml")).getFile());
-/// /        assertTrue(xmlFile.exists());
-/// /        assertTrue(xmlFile.canRead());
-/// /
-/// /        XmlParser xmlParser = xmlMapper.readValue(xmlFile, XmlParser.class);
-/// /
-/// /        assertTrue(Objects.nonNull(xmlParser));
-/// /    }
+//    @Test
+//    public void testXmlParserFromFile() throws IOException {
+//        File xmlFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("transactionsxml")).getFile());
+//        assertTrue(xmlFile.exists());
+//        assertTrue(xmlFile.canRead());
+//
+//        XmlParser xmlParser = xmlMapper.readValue(xmlFile, XmlParser.class);
+//
+//        assertTrue(Objects.nonNull(xmlParser));
+//    }
 //}
